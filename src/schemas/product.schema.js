b884008e36fd5132f110181c5eb299440b05ac44
Sebazt <juan.londono29@udea.edu.co>
2023-03-28 23:41:49 -0500

    :rocket: add joi

---
+++ src/schemas/product.schema.js
@@ -0,0 +1,27 @@
+// la responsabilidad de este schema es validar la data que nos envian desde el cliente
+
+const Joi = require('joi');
+
+const id = Joi.string().uuid();
+const name = Joi.string().alphanum().min(3).max(20);
+const price = Joi.number().integer().min(10);
+
+const createProductSchema = Joi.object({
+  name: name.required(),
+  price: price.required(),
+});
+
+const updateProductSchema = Joi.object({
+  name: name,
+  price: price,
+});
+
+const getProductSchema = Joi.object({
+  id: id.required(),
+});
+
+module.exports = {
+  createProductSchema,
+  updateProductSchema,
+  getProductSchema
+}
